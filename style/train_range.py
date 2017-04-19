2fffd53bc0e94bc8d9d935e851c774524d40d0fe
evgenyzamyatin <jeka_zamyatin@mail.ru>
2017-04-19 23:42:53 +0300

    train_range

---
+++ style/train_range.py
@@ -0,0 +1,43 @@
+import subprocess
+import numpy as np
+import argparse
+import os
+
+import shutil
+
+
+def main(args):
+    params = {
+        '--train-dir': './data/train2014',
+        '--val-dir': './data/train2014',
+        '--test-image': './data/images/ghc.jpg',
+        '--test-size': '1024',
+        '--checkpoint': '',
+        '--style-image': args.style_image,
+        '--train-iter': '10000'
+    }
+    for i, sw in enumerate(np.linspace(args.start, args.end, args.n)):
+        params['--style-weight'] = sw
+        output = args.output + '/%d' % i
+        params['--output-dir'] = output
+        if i != 0:
+            params['--model'] = args.output + '/%d' % (i-1) + '/model.h5'
+        subprocess.call('python3 fast_neural_style/fast_neural_style.py train ' + ' '.join(' '.join(item) for item in params.items()),
+                        shell=True)
+    result_dir = './data/models/%s' % args.style_image
+    
+    os.makedirs(result_dir)
+    for i in range(args.n):
+        shutil.copy(args.output + '/%d' % i + '/model.h5', result_dir + '/%d.h5')
+
+
+if __name__ == '__main__':
+    main_arg_parser = argparse.ArgumentParser()
+    main_arg_parser.add_argument("--style-image", type=str, required=True, help='Path to style image')
+    main_arg_parser.add_argument("--output", type=str, required=True, help='Path to output dir')
+    main_arg_parser.add_argument("--start", type=float, required=True, help='Start style weight')
+    main_arg_parser.add_argument("--end", type=float, required=True, help='End style weight')
+    main_arg_parser.add_argument("--n", type=int, required=True, help='Count')
+
+    args = main_arg_parser.parse_args()
+    main(*args)
