4e3cf0b9413a5789f182accc983fd1650a7950aa
evgenyzamyatin <jeka_zamyatin@mail.ru>
2017-04-19 23:48:24 +0300

    train_range

--- style/train_range.py
+++ style/train_range.py
@@ -14,7 +14,7 @@ def main(args):
         '--test-size': '1024',
         '--checkpoint': '',
         '--style-image': args.style_image,
-        '--train-iter': '10000'
+        '--train-iter': '100'
     }
     for i, sw in enumerate(np.linspace(args.start, args.end, args.n)):
         params['--style-weight'] = str(sw)
