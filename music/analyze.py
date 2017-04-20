140850b0d5c12570fb5088eb616a61b123ffe3ac
evgenyzamyatin <jeka_zamyatin@mail.ru>
2017-04-20 15:28:37 +0300

    audio amalyze change

--- music/analyze.py
+++ music/analyze.py
@@ -6,7 +6,7 @@ from scipy.signal import savgol_filter
 from utils import readAudioFile
 
 
-def analyze(audio_file, frames_count):
+def analyze1(audio_file, frames_count):
     audio = readAudioFile(audio_file)
     audio = np.abs(audio)
     step = int(np.ceil(len(audio) / frames_count))
@@ -25,23 +25,24 @@ def analyze(audio_file, frames_count):
     return fs
 
 
-def analyze_1(audio_file, frames_count):
+def analyze(audio_file, frames_count):
     audio = readAudioFile(audio_file)
     audio = np.abs(audio)
     step = int(np.ceil(len(audio) / frames_count))
     fs = []
     for i in range(0, len(audio), step):
         chunk = audio[i:i + step]
-        fft = np.fft.fft(chunk, 2048)
+        fft = np.fft.fft(chunk, 1024)
         t = np.absolute(fft)
         t = (t[:len(t)//2] + (t[len(t)//2:])) / 2
-        fs.append(np.trapz(t[8:14]))
+        fs.append(np.trapz(t[4:7]))
     fs = np.array(fs)
-    #fs = (fs - fs.min()) / (fs.max() - fs.min())
+    fs = (fs - fs.min()) / (fs.max() - fs.min())
+
     #fs[fs < 0.7] /= 5
     #fs[fs >= 0.7] *= 2
     #fs = savgol_filter(fs, 7, 3)
-    fs = savgol_filter(fs, 9, 5)
+    #fs = savgol_filter(fs, 9, 5)
     fs = (fs-fs.mean()) / np.sqrt(fs.var() + 1e-5)
     fs[fs < 0] = 0
     #fs = savgol_filter(fs, 5, 3)
@@ -51,24 +52,27 @@ def analyze_1(audio_file, frames_count):
 
     for i in range(1, len(fs)):
         if fs[i] > fs[i - 1]: continue
-        fs[i] = max(fs[i], fs[i - 1] - 0.035)
+        fs[i] = max(fs[i], fs[i - 1] - 0.04)
 
     for i in range(0, len(fs)-1):
         if fs[i+1] < fs[i]: continue
-        fs[i+1] = min(fs[i] + 0.035, fs[i + 1])
+        fs[i+1] = min(fs[i] + 0.04, fs[i + 1])
 
     #fs = savgol_filter(fs, 3, 1)
     #fs = (fs - fs.mean())
     #fs[fs < 0] = 0
-    #fs = (fs - fs.min()) / (fs.max() - fs.min())
+    fs = (fs - fs.min()) / (fs.max() - fs.min())
 
     return fs
 
 
 def main(args):
-    audio_analyze = analyze(args.audio, args.frames)
     import matplotlib.pyplot as plt
+    audio_analyze = analyze(args.audio, args.frames)
+    audio_analyze_1 = analyze_1(args.audio, args.frames)
+
     plt.plot(audio_analyze)
+    plt.plot(audio_analyze_1)
     plt.show()
 
 
